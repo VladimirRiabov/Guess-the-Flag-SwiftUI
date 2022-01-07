4b366f22a959bb843bb85cdf0cfb22a02b17c5c7
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-07 21:24:15 +0300

    Initial Commit

---
+++ Guess the Flag SwiftUI/Guess_the_Flag_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_Flag_SwiftUIApp.swift
+//  Guess the Flag SwiftUI
+//
+//  Created by Владимир Рябов on 07.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_Flag_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
