3b74cf8f0789051ca84c3046c0316019dfd296d7
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-07 22:00:46 +0300

    Colors and frames – Guess the Flag SwiftUI Tutorial 2/9

--- Guess the Flag SwiftUI/ContentView.swift
+++ Guess the Flag SwiftUI/ContentView.swift
@@ -9,24 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Text("1")
-                Text("2")
-                Text("3")
+        ZStack() {
+            VStack(spacing: 0) {
+                Color(red: 1, green: 0.5, blue: 0.1)
+                
+                Color(red: 1, green: 0.3, blue: 0.4)
+                   
             }
-            HStack {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            HStack {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
-            
-        }
+            Text("Some text")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+        }.ignoresSafeArea()
     }
 }
 
