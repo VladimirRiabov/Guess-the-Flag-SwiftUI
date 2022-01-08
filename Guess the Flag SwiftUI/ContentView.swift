2b12b88669bf385dffc9f3032014ab34811679ef
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-08 11:38:55 +0300

    Gradients – Guess the Flag SwiftUI Tutorial 3/9

--- Guess the Flag SwiftUI/ContentView.swift
+++ Guess the Flag SwiftUI/ContentView.swift
@@ -11,10 +11,21 @@ struct ContentView: View {
     var body: some View {
         ZStack() {
             VStack(spacing: 0) {
-                Color(red: 1, green: 0.5, blue: 0.1)
+                LinearGradient(stops: [
+                    Gradient.Stop(color: .blue, location: 0.2),
+                    Gradient.Stop(color: .yellow, location: 0.7),
+                ], startPoint: .topLeading, endPoint: .bottomLeading)
                 
-                Color(red: 1, green: 0.3, blue: 0.4)
-                   
+                RadialGradient(colors: [.black, .purple], center: .center, startRadius: 30, endRadius: 300)
+                
+                AngularGradient(colors: [.red, .blue, .white, .black, .purple, .red], center: .center)
+                
+                LinearGradient(stops: [
+                    Gradient.Stop(color: .red, location: 0.1),
+                    Gradient.Stop(color: .blue, location: 0.7),
+                ], startPoint: .topLeading, endPoint: .bottomLeading)
+                
+            
             }
             Text("Some text")
                 .foregroundStyle(.secondary)
